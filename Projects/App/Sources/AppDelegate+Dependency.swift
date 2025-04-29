b9d0125fc6098b16246cf2eadeeac86f118b4904
Den <qudgus1984@naver.com>
2025-04-29 14:13:48 +0900

    Design: Font, Color 설정 완료

---
+++ Projects/App/Sources/AppDelegate+Dependency.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate+Dependency.swift
+//  App
+//
+//  Created by Den on 4/29/25.
+//  Copyright © 2025 Den. All rights reserved.
+//
+
+import Foundation
+
+import Utility
+
+extension AppDelegate {
+    func registerDependencies() {
+        
+    }
+}
